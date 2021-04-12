257807330ccaf06f7dc1dca5622fbccb2a235333
Kallel <kallel.anobom@agriness.com>
2021-04-12 10:55:52 -0300

    Created components for used in app

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from 'style/Header.module.css'
+
+const Header = () => {
+  return (
+    <header className={styles.Header}>
+      <h1 className={styles.HeaderTitle}>StackOverflow</h1>
+    </header>
+  )
+}
+
+export default Header
